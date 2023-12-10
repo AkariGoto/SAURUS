63c94b0a3c9e1d928a31ab3c2b91c1c4b9013843
AkariGoto <76420233+AkariGoto@users.noreply.github.com>
2023-12-10 18:28:22 +0900

    Create main.h

---
+++ note/main.h
@@ -0,0 +1,14 @@
+/*
+ * init_asuraII.h
+ *
+ *  Created on: 2018/07/09
+ *      Author: yoshida
+ */
+
+#ifndef SRC_INIT_ASURA_H_
+#define SRC_INIT_ASURA_H_
+
+/* Private function prototypes -----------------------------------------------*/
+void init_asuraII(void);
+
+#endif /* SRC_INIT_ASURA_H_ */
