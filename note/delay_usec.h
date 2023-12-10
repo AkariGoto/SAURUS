8278e653d9f1a0e96512cb948c1c94111c77aeab
AkariGoto <76420233+AkariGoto@users.noreply.github.com>
2023-12-10 18:27:14 +0900

    Create delay_usec.h

---
+++ note/delay_usec.h
@@ -0,0 +1,17 @@
+/*
+ * delay_usec.h
+ *
+ *  Created on: 2018/02/23
+ *      Author: yoshida
+ */
+
+#ifndef SRC_DELAY_USEC_H_
+#define SRC_DELAY_USEC_H_
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private function prototypes -----------------------------------------------*/
+void delay_usec(uint32_t waittime);
+
+#endif /* SRC_DELAY_USEC_H_ */
