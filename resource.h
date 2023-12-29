304b167d0322f22cef03d5eeaf1ef77f4a86a9a4
AkariGoto <76420233+AkariGoto@users.noreply.github.com>
2023-12-29 18:00:21 +0900

    Add files via upload

---
+++ resource.h
@@ -0,0 +1,102 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ で生成されたインクルード ファイル。
+// ASURA2GUI.rc で使用
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDD_ASURA2GUI_FORM              101
+#define IDR_MAINFRAME                   128
+#define IDR_ASURA2GUITYPE               130
+#define IDD_DLG_AXIS_CONTROL            310
+#define IDD_DLG_SERIAL_PORT             312
+#define DC_MAIN_SCREEN                  1000
+#define IDC_MAIN_SCREEN                 1000
+#define IDC_CHECK_DRIVE                 1001
+#define IDC_CHECK_COMM                  1002
+#define IDC_CHECK_LOG                   1003
+#define IDC_CHECK_LEG2                  1003
+#define IDC_EDIT1                       1004
+#define IDC_TIME                        1004
+#define IDC_CHECK_LEG3                  1004
+#define IDC_SENDING_EDIT                1004
+#define IDC_EDIT_STRIDE                 1004
+#define IDC_EDIT_JOINT1                 1005
+#define IDC_JOINT1                      1005
+#define IDC_CRAB_ANGLE                  1005
+#define IDC_SENDING_EDIT2               1005
+#define IDC_RECEIVING_EDIT              1005
+#define IDC_EDIT_JOINT2                 1006
+#define IDC_JOINT2                      1006
+#define IDC_CONTROL_MODE                1006
+#define IDC_EDIT_JOINT3                 1007
+#define IDC_JOINT3                      1007
+#define IDC_CHECK_LEG4                  1007
+#define IDC_EDIT_JOINT4                 1008
+#define IDC_JOINT4                      1008
+#define IDC_CHECK_LEG1                  1008
+#define IDC_EDIT_ACT1                   1009
+#define IDC_ACT1                        1009
+#define IDC_CHECK_POSITION              1009
+#define IDC_EDIT_ACT2                   1010
+#define IDC_ACT2                        1010
+#define IDC_CHECK_VELOCITY              1010
+#define IDC_EDIT_ACT3                   1011
+#define IDC_ACT3                        1011
+#define IDC_SLIDER_JOINT1               1011
+#define IDC_EDIT_ACT4                   1012
+#define IDC_ACT4                        1012
+#define IDC_SLIDER_JOINT2               1012
+#define IDC_EDIT_FOOTX                  1013
+#define IDC_FOOTX                       1013
+#define IDC_SLIDER_JOINT3               1013
+#define IDC_EDIT_FOOTY                  1014
+#define IDC_FOOTY                       1014
+#define IDC_SLIDER_FOOTX                1014
+#define IDC_EDIT_JOINT11                1015
+#define IDC_EDIT_FOOTZ                  1015
+#define IDC_FOOTZ                       1015
+#define IDC_SLIDER_FOOTY                1015
+#define IDC_SLIDER_FOOTZ                1016
+#define IDC_FOOTZ2                      1016
+#define IDC_ELAPSED_TIME                1016
+#define IDC_EDIT2                       1017
+#define IDC_EDIT_WALKING_SPEED          1017
+#define IDC_EDIT3                       1018
+#define IDC_BUTTON1                     1018
+#define IDC_BUTTON_SET                  1018
+#define IDC_EDIT4                       1019
+#define IDC_EDIT_SWING_UP               1019
+#define IDC_EDIT5                       1020
+#define IDC_EDIT_STRIDE4                1020
+#define IDC_EDIT_CYCLE_TIME             1020
+#define IDC_EDIT6                       1021
+#define IDC_EDIT_SWING_UP2              1021
+#define IDC_EDIT_SWING_DOWN             1021
+#define IDC_EDIT7                       1022
+#define IDC_CHECK1                      1022
+#define IDC_CHECK_1_CYCLE               1022
+#define IDC_JOINT_4_2                   1023
+#define IDC_JOINT4_2                    1023
+#define IDC_RADIO_WORLD                 1024
+#define IDC_RADIO2                      1025
+#define IDC_RADIO_LEG                   1025
+#define ID_MODE_PLANNEDMOTION           32771
+#define ID_PLANNEDMOTION_TRIPODGAIT     32772
+#define ID_DISPLAY_SERIALPORTLOG        32773
+#define ID_DISPLAY_JOINTAXISCONTROL     32774
+#define ID_PLANNEDMOTION_               32775
+#define ID_TRACK_PLAN                   32776
+#define ID_PLANNEDMOTION_TROTGAIT       32777
+#define ID_SHOW_SERIAL_PORT_DLG         32778
+#define ID_SHOW_AXIS_CTRL_DLG           32779
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        314
+#define _APS_NEXT_COMMAND_VALUE         32780
+#define _APS_NEXT_CONTROL_VALUE         1026
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
